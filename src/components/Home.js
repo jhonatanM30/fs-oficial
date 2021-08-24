8094fb4c05aab016175ae498df59e491944479ca
jcuesta2 <jcuesta@cidenet.com.co>
2021-08-24 15:09:50 -0500

    Se ajusta de manera responsive el componente Home

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,31 +1,35 @@
 import React from "react";
-import { 
+import {
     Link
-  } from "react-router-dom";
+} from "react-router-dom";
 import "../css/home.css";
 
 export const Home = () => {
     return (
-        <div className="cont">                 
-            <div className="hero">
-                <div className="container">
-                    <h1>Bienvenido A <span>La Liga De Fútbol</span></h1>
-                    <p>En la plataforma de la liga de fútbol te presentamos lo mejor para ti y tu equipo, visita las diferentes opciones y hazte un gol actualizando la información de tu interes</p>
-                    <button className="arrow">
-                        <i className="fas fa-arrow-down fa-5x"></i>                        
-                    </button> <br></br> 
-                    <div className="btn-link">
-                    <Link to="/jugadores" className="btn-go">
-                        Lo mejor de la fecha 📺                      
-                    </Link>  
-                    <Link to="/jugadores" className="btn-go">
-                       Gol de la fecha ⚽                      
-                    </Link>  
-                    <Link to="/jugadores" className="btn-go">
-                       Jugador de la fecha 🥇                      
-                    </Link>  
-                    </div>                                   
+        <div className="container cont-home">
+            <div className="row">
+                <div className="col">
+                    <h1 className="titleHome">Bienvenido A <span>La Liga De Fútbol</span></h1>
                 </div>
+            </div>
+            <div className="row">
+                <div className="col">
+                    <p>En la plataforma de la liga de fútbol te presentamos lo mejor para ti y tu equipo, visita las diferentes opciones y hazte un gol actualizando la información de tu interes</p>
+                </div>
+            </div>            
+            <div className="row justify-content-lg-center justify-content-sm-center">
+                <div className="col-sm-6 col-lg-3  g-0 ">
+                    <Link to="/jugadores" className="buttonUD">
+                        Lo mejor de la fecha 📺
+                    </Link></div>
+                <div className="col-sm-6 col-lg-3 g-0">
+                    <Link to="/jugadores" className="buttonUD">
+                        Gol de la fecha ⚽
+                    </Link> </div>
+                <div className="col-sm-6 col-lg-3  g-0">
+                    <Link to="/jugadores" className="buttonUD">
+                        Jugador de la fecha 🥇
+                    </Link> </div>
             </div>
         </div>
     );
